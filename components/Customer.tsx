8c7c366fc0ee65af13ac85af14b34f5e3495bb66
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2021-01-15 12:12:51 +1100

    no more errors

--- components/Customer.tsx
+++ components/Customer.tsx
@@ -42,7 +42,7 @@ export default function CustomerComponent({
             href="/customers/[id]"
             as={`/customers/${customer.id}`}
           >
-            {customer?.name.length > 0 ? customer?.name : customer?.email}
+            {customer.name?.length > 0 ? customer?.name : customer?.email}
           </NextChakraLink>
         </Center>
         <Spacer />
